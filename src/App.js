8f1ab2a19d68f8d7fd5f817f26203607e97d5036
darksoul <jawadmirza606@gmail.com>
2022-01-21 08:30:06 +0500

    Cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import './App.css';
-import {Navbar,NavbarBrand} from 'reactstrap';
-import Menu from './components/menucomponents';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Navbar dark color='primary'>
-        <div className="container">
-          <NavbarBrand href='/' >Jawad Mirza</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-      </header>
-    
-    </div>
-  );
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import React, { Component } from "react";
+import Menu from "./components/menucomponents";
+import { DISHES } from "./shared/Dishes";
+
+class App extends Component {
+
+  
+  constructor (props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Jawad Mirza</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
