17491fdaaa6d33a42808b441d3b564383e4c4ffb
darksoul <jawadmirza606@gmail.com>
2022-01-21 10:40:11 +0500

    With Dish Comment

--- src/components/menucomponents.js
+++ src/components/menucomponents.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardBody,
-  CardText,
-  CardTitle,
-  CardImg,
-} from "reactstrap";
+import { Card, CardBody, CardText, CardTitle, CardImg } from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
@@ -14,10 +8,11 @@ class Menu extends Component {
       selecteddish: null,
     };
   }
+
+  
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
   }
-
   renderDish(dish) {
     if (dish != null)
       return (
@@ -31,11 +26,53 @@ class Menu extends Component {
       );
     else return <div></div>;
   }
+
+  
+  rendercomments(dish) {
+    if (dish != null){
+      return (
+        <Card>
+            <h3>Comments</h3>
+          <CardBody>
+            <CardText>{dish.comments[0].comment}</CardText>
+            <CardTitle>{dish.comments[0].author}</CardTitle>
+            <CardTitle>{dish.comments[0].date}</CardTitle>
+          </CardBody>
+          <CardBody>
+            <CardText>{dish.comments[1].comment}</CardText>
+            <CardTitle>{dish.comments[1].author}</CardTitle>
+            <CardTitle>{dish.comments[1].date}</CardTitle>
+          </CardBody>
+          <CardBody>
+            <CardText>{dish.comments[2].comment}</CardText>
+            <CardTitle>{dish.comments[2].author}</CardTitle>
+            <CardTitle>{dish.comments[2].date}</CardTitle>
+          </CardBody>
+          <CardBody>
+            <CardText>{dish.comments[3].comment}</CardText>
+            <CardTitle>{dish.comments[3].author}</CardTitle>
+            <CardTitle>{dish.comments[3].date}</CardTitle>
+          </CardBody>
+          <CardBody>
+            <CardText>{dish.comments[4].comment}</CardText>
+            <CardTitle>{dish.comments[4].author}</CardTitle>
+            <CardTitle>{dish.comments[4].date}</CardTitle>
+          </CardBody>
+         
+        </Card>
+      );}
+    else return <div></div>;
+
+  }
+ 
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 col-md-5 m-5 d-flex flex-row">
-          <Card style={{ width: "18rem" }} onClick={() => this.onDishSelect(dish)} >
+        <div key={dish.id} className="col-12 col-md-5 col-sm-5 m-5 ">
+          <Card
+            style={{ width: "18rem" }}
+            onClick={() => this.onDishSelect(dish)}
+          >
             <CardImg variant="top" src={dish.image} alt={dish.name} />
           </Card>
         </div>
@@ -43,13 +80,19 @@ class Menu extends Component {
     });
 
     return (
-      <div className="container">
-        <div className="row">{menu}</div>
+      <div className="container ">
+        <div className="row ">{menu}</div>
         <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+          <div className="col-6 col-md-5 col-sm-5 m-1">
+            {this.renderDish(this.state.selectedDish)}
+          </div>
+          <div className="col-6 col-md-5 col-sm-5 m-1">
+            {this.rendercomments(this.state.selectedDish)}
+          </div>
+        </div>
+        <div className="row">
+          
+        </div>
       </div>
     );
   }
